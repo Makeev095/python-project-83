34e674b6a9b4e6e98d88de8d536a620cd2b39eea
Makeev095 <makeev095095@mail.ru>
2023-02-18 20:05:51 +0300

    step_1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,5 @@
 from flask import Flask
-import os
-from dotenv import load_dotenv
 
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
